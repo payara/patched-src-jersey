d1243cfc6ee63efb7c1c5824bb1e526a19f15835
Arie Kiswanto <arie.kiswanto@payara.fish>
2022-11-14 23:21:21 +0800

    FISH-6670 Update copyright

--- core-common/src/main/java/org/glassfish/jersey/internal/inject/Providers.java
+++ core-common/src/main/java/org/glassfish/jersey/internal/inject/Providers.java
@@ -13,6 +13,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
+// Portions Copyright 2022 [Payara Foundation and/or its affiliates]
 
 package org.glassfish.jersey.internal.inject;
 
